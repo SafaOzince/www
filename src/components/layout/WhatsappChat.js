225acd2043006df42c1c3196028546e3e2a9ca7a
SafaOzince <safabaturalpozince@gmail.com>
2022-12-15 10:56:36 +0300

    whatsapplogoupdate

--- src/components/layout/WhatsappChat.js
+++ src/components/layout/WhatsappChat.js
@@ -1,19 +1,17 @@
 import React from "react";
 import Link from "react-router-dom";
 import { RedirectToWhatsAppPage } from "../../helpers/CommonHelper";
-import WhatsappLogo from "../images/whatsapp-logo.png";
+import WhatsappLogo from "../images/icons8-whatsapp-70.png";
 
 const WhatsappChat = () => {
 
     return(
-        // <div className="whatsapp">
         <a href="#!" className="whatsapp_float" onClick={(e)=>{
             e.preventDefault();
             RedirectToWhatsAppPage();
         }}>
             <img src={WhatsappLogo}/>
         </a>
-        // </div>
     )
 }
 
